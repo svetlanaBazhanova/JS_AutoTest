7bde38ac1e587df60f98a6141269fbec11000e71
Ilya Kuydich <agree2012@yandex.ru>
2017-06-05 16:54:02 +0400

    Update conf.js

--- conf.js
+++ conf.js
@@ -2,7 +2,7 @@ var Jasmine2HtmlReporter = require('protractor-jasmine2-html-reporter');
 exports.config = {
     specs: ['tests/*.js'],
     multiCapabilities: [
-        {'browserName': 'chrome'}
+        {'browserName': 'phantomjs'}
     ],
 	onPrepare: function() {   
 	jasmine.getEnv().addReporter(        new Jasmine2HtmlReporter({          savePath: 'target/screenshots'      }));  
